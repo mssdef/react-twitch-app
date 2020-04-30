05c632142e321d69889a965963563c704d000295
Andrew <andrewrr@Andrejs-MacBook-Air.local>
2020-04-30 15:45:52 +0300

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
-
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-}
-
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-}
-
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
-  return <div>
-    <BrowserRouter>
-      <Route path="/1" exact component={PageOne} />
-      <Route path="/2" exact component={PageTwo} />
+  return <BrowserRouter>
+      <div>
+        <Header/>
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/" exact component={StreamList} />
+      </div>
     </BrowserRouter>
-  </div>
 }
 
 export default App;
