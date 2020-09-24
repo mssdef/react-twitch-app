9efa54d30b8f8eabb567ffebcdbfa8ca03aae492
Andrew <andrew@masterdef.net>
2020-09-24 11:55:45 +0300

    search form with term

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
-  return <h1>Search cmp</h1>
+  const [term, setTerm] = useState('');
+
+  return <div>
+    <div className="ui form">
+      <div className="field">
+        <label>Enter Search Term</label>
+        <input value={term}
+          onChange={e => setTerm(e.target.value)}
+         className="input" />
+      </div>
+    </div>
+  </div>
 }
 
 export default Search;
