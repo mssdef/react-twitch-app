b516f35b13f6e78b93a6c64fa1fd2f047547c94d
Andrew <andrew@masterdef.net>
2020-10-05 15:38:30 +0300

    Display search results list

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const renderedResults = results.map((res, i) => {
+      return <div className="item" key={i}>
+        <div className="content">
+          <div className="header">
+            {res.title}
+          </div>
+          {res.snippet}
+        </div>
+      </div>
+  });
 
   useEffect(() => {
+      const search = async () => {
+        const { data } =  await axios.get('https://en.wikipedia.org/w/api.php', {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term
+          }
+        });
 
+        setResults(data.query.search);
+      }
+
+      if (term) search();
   }, [term]);
 
   return <div>
@@ -15,6 +42,10 @@ const Search = () => {
           onChange={e => setTerm(e.target.value)}
          className="input" />
       </div>
+    </div>
+
+    <div className="ui celled list">
+      {renderedResults}
     </div>
   </div>
 }
