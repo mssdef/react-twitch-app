a000f054943b7f545cc5fb51076e3efdf9f6f23a
Andrew <masterdef@gmail.com>
2021-01-18 16:00:13 +0200

    upd

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -36,23 +36,23 @@
     return <div>
       <Header />
 
-      <>
-      <Route path="/">
-        <Accordion items={items} />
-      </Route>
-      <Route path="/list">
-        <Search />
-      </Route>
-      <Route path="/dropdown">
-        <Dropdown label="Select a color..."
-            options={options}
-            selected={selected}
-            onSelectedChange={setSelected}
-          />
-      </Route>
-      <Route path="/translate">
-        <Translate />
-      </Route>
-      </>
+      <Switch>
+        <Route exact path="/">
+          <Accordion items={items} />
+        </Route>
+        <Route path="/list">
+          <Search />
+        </Route>
+        <Route path="/dropdown">
+          <Dropdown label="Select a color..."
+              options={options}
+              selected={selected}
+              onSelectedChange={setSelected}
+            />
+        </Route>
+        <Route path="/translate">
+          <Translate />
+        </Route>
+      </Switch>
     </div>;
  }
